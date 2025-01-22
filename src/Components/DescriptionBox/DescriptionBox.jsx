ababad2e7833f64e4036a201243bbd5b806cefd2
Chaitanya Nikose <chaitanyanikose4@gmail.com>
2025-01-22 16:40:18 +0530

    first commit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../DescriptionBox/DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='description'>
+     <div className='description-box-navigator'>
+        <div className="description-box-nav-box">Description</div>
+        <div className="description-box-nav-fade"> Reviews(122)</div>
+     </div>
+<div className="description-box-description">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non veniam perferendis, corrupti similique 
+        unde blanditiis quas fugit tempora quia eaque. Eaque, praesentium quidem? Quisquam, explicabo autem 
+        optio sapiente aspernatur ipsam voluptatum, praesentium saepe repudiandae delectus, quibusdam impedit 
+        obcaecati ea minus deleniti ex? Vitae officiis consequuntur dolorem pariatur. Adipisci praesentium unde
+         obcaecati sint ut laudantium cumque. Dicta ab minima tenetur rem vitae animi error non magni omnis 
+         laboriosam ex quam dolor dignissimos expedita iure deserunt quidem voluptates repellendus beatae sed 
+         doloremque exercitationem, cupiditate provident illo. Iusto, iste ab est quaerat aperiam labore suscipit
+          cupiditate voluptates sapiente hic, voluptatibus commodi excepturi nostrum.</p>
+</div>
+    </div>
+  )
+}
+
+export default DescriptionBox
